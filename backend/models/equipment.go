4f2865a3d619137a18c5aa8a23de299c4ea28db9
top002200 <142074454+top002200@users.noreply.github.com>
2024-11-12 15:14:21 +0700

    type

--- backend/models/equipment.go
+++ backend/models/equipment.go
@@ -1,9 +1,10 @@
 package models
 
 type Equipment struct {
-	EquipmentID      uint         `gorm:"primaryKey;autoIncrement" json:"equipment_id"`
-	EquipmentName    string       `json:"equipment_name"`
-	CreatedByAdminID string       `json:"created_by_admin_id"` // ระบุ foreign key ตรงกับ AdminID
-	ProfilePic       string       `json:"profile_pic"`
-	Submissions      []Submission `gorm:"foreignKey:EquipmentID;references:EquipmentID" json:"submissions"`
+	EquipmentID   uint         `gorm:"primaryKey;autoIncrement" json:"equipment_id"`
+	EquipmentName string       `json:"equipment_name"`
+	Type          string       `json:"equipment_type"`
+	Model         string       `json:"equipment_model"`
+	ProfilePic    string       `json:"profile_pic"`
+	Submissions   []Submission `gorm:"foreignKey:EquipmentID;references:EquipmentID" json:"submissions"`
 }
